8356d56630d752492469e2a92be091d3ffd04661
Nakul <nakul.b@flyin.com>
2025-08-15 18:16:46 +0530

    Made the Databse related changes and addedd the credentials for SQL

--- src/main/java/com/eCom/eCommerce/Models/Order.java
+++ src/main/java/com/eCom/eCommerce/Models/Order.java
@@ -2,6 +2,15 @@ package com.eCom.eCommerce.Models;
 
 import java.sql.Date;
 
+import com.eCom.eCommerce.User.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +18,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
-    private String userId;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
     private String productId;
     private String productName;
     private Date orderDate;
--- src/main/java/com/eCom/eCommerce/User/User.java
+++ src/main/java/com/eCom/eCommerce/User/User.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import com.eCom.eCommerce.Models.Order;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,11 +20,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String userId;
+    private Long userId;
 
     @Column(nullable = false)
     private String userName;
@@ -36,5 +38,6 @@ public class User {
     @Column(nullable = false)
     private String Address;
 
-    private List<Order> order;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
 }
